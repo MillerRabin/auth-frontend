61780918df15f999bdd60c364f64de47127d4180
millerrabin <nemesisv@mail.ru>
2020-01-18 14:52:58 +0300

    added id generation at ui side

--- apps/KeyGen/KeyGen.js
+++ apps/KeyGen/KeyGen.js
@@ -1,12 +1,14 @@
 import loader from "/node_modules/async-content-loader/main.js";
+import UUID from "/node_modules/cross-uuid/browser/main.js";
 
 const gTemplateP = loader.request(`/apps/KeyGen/KeyGen.html`);
 
 async function render(keygen) {
     const template = await gTemplateP;
     keygen._mount.innerHTML = template.text;
+    keygen._id = keygen._mount.querySelector('.line.id .value');
+    keygen.id = UUID.generate();
 }
-
 
 export default class KeyGen {
     constructor (mount) {
@@ -16,5 +18,14 @@ export default class KeyGen {
 
     get mount() {
         return this._mount;
+    }
+
+    get id() {
+        if (this._id == null) return null;
+        this._id.innerHTML;
+    }
+
+    set id(value) {
+        this._id.innerHTML = value;
     }
 }
